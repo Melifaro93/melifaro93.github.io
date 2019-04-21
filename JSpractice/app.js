c66c8ad07515c7a0bef9351174993751c102031a
Oleg Gordienko <melifarosaint@gmail.com>
2019-04-21 20:34:35 +0300

    Add New Folder JSpractice with home work about massives

---
+++ JSpractice/app.js
@@ -0,0 +1,21 @@
+
+/*var arr = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+function arraySum(array){
+var sum = 0;
+for(var i = 0; i < array.length; i++){
+    sum += array[i];
+    }
+console.log(sum);
+}
+arraySum(arr);
+*/
+
+
+
+var arr = [3,68,4,35,5,6,17,8,2061,100,11,109];
+
+const min = Math.min(...arr)
+const max = Math.max(...arr)
+
+console.log("найменьшее число " + min,"найбольшее число " + max)  
